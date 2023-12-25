ecdefd763ed79eceaf49db1bd6c310e6ce8e2260
Rogério Senna [MacTW] <rogerio.senna@thoughtworks.com>
2023-12-25 18:15:01 +0100

    editorconfig: use 2 spaces for indent; fix build errors

--- main.ts
+++ main.ts
@@ -1,179 +1,109 @@
-import { EditLinkModal } from 'EditLinkModal';
-import { LinkType } from 'Link';
-import { link } from 'fs';
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface SupernotesPluginSettings {
-	apiKey: string;
-}
-
-const DEFAULT_SETTINGS: SupernotesPluginSettings = {
-	apiKey: 'default'
-}
+import { App, Editor, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
+import { DEFAULT_SETTINGS } from 'settings';
+import { SupernotesPluginSettings } from 'settings';
+import { SupernotesSettingTab } from 'settings';
 
 export default class SupernotesPlugin extends Plugin {
-	settings: SupernotesPluginSettings;
+  settings: SupernotesPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // This creates an icon in the left ribbon.
+    const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+      // Called when the user clicks the icon.
+      new Notice('This is a notice!');
+    });
+    // Perform additional things with the ribbon
+    ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarItemEl = this.addStatusBarItem();
+    statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app, 'Simple').open();
-			}
-		});
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: 'open-sample-modal-simple',
+      name: 'Open sample modal (simple)',
+      callback: () => {
+        new SampleModal(this.app, 'Simple').open();
+      }
+    });
 
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: 'sample-editor-command',
+      name: 'Sample editor command',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection());
+        editor.replaceSelection('Sample Editor Command');
+      }
+    });
 
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					this.app.workspace.activeEditor?.editor?.getSelection
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: 'open-sample-modal-complex',
+      name: 'Open sample modal (complex)',
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (markdownView) {
+          this.app.workspace.activeEditor?.editor?.getSelection
 
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app, 'Whoa!').open();
-					}
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            new SampleModal(this.app, 'Whoa!').open();
+          }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      }
+    });
 
-		this.addCommand({
-			id: 'supernotes-edit-link',
-			name: 'Edit Link',
-			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
-				const result = true;
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SupernotesSettingTab(this.app, this));
 
-				if (checking) {
-					return result;
-				}
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+      console.log('click', evt);
+    });
 
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+  }
 
-				// TODO:
-				// 1. Get the link under the cursor
-				//   - It can be difficult to get the link under the cursor, because
-				//     1. The cursor can be in any part of the link
-				//     2. The link can be a wikilink or a markdown link
-				//     3. The link can have an alias or not
-				//     4. The link can have a link address or not
-				//     5. ~The link can have multiple lines of text~ (not true)
-				// 2. Open a modal with the link's text and address
-				// 3. Update the link's text and address when the modal is closed
-				//
-				// Notes:
-				// From the Obsidian console, you can get the editor and cursor with:
-				// const editor = this.app.workspace.activeEditor.editor;
-				// const cursor = editor.getCursor();
+  onunload() {
 
+  }
 
-				const editLinkModal = new EditLinkModal(this.app, editor, () => {});
-				editLinkModal.open();
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-				return result;
- 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SupernotesSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	text: string;
+  text: string;
 
-	constructor(app: App, text: string) {
-		super(app);
-		this.text = text;
-	}
+  constructor(app: App, text: string) {
+    super(app);
+    this.text = text;
+  }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText(this.text);
-	}
+  onOpen() {
+    const {contentEl} = this;
+    contentEl.setText(this.text);
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  onClose() {
+    const {contentEl} = this;
+    contentEl.empty();
+  }
 }
 
-class SupernotesSettingTab extends PluginSettingTab {
-	plugin: SupernotesPlugin;
-
-	constructor(app: App, plugin: SupernotesPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Supernotes API Key')
-			.setDesc('Key used for Obsidian to control Supernotes\' database')
-			.addText(text => text
-				.setPlaceholder('Enter your API Key')
-				.setValue(this.plugin.settings.apiKey)
-				.onChange(async (value) => {
-					this.plugin.settings.apiKey = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
---
+++ settings.ts
@@ -0,0 +1,49 @@
+import SupernotesPlugin from 'main';
+
+import { PluginSettingTab, App, Setting } from "obsidian";
+
+export interface SupernotesPluginSettings {
+  apiKey: string;
+  folder: string;
+}
+export const DEFAULT_SETTINGS: SupernotesPluginSettings = {
+  apiKey: 'set API key here',
+  folder: '/'
+};
+
+export class SupernotesSettingTab extends PluginSettingTab {
+  plugin: SupernotesPlugin;
+
+  constructor(app: App, plugin: SupernotesPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName('Supernotes API Key')
+      .setDesc('Key used for Obsidian to control Supernotes\' database')
+      .addText(text => text
+        .setPlaceholder('Enter your API Key')
+        .setValue(this.plugin.settings.apiKey)
+        .onChange(async (value) => {
+          this.plugin.settings.apiKey = value;
+          await this.plugin.saveSettings();
+        }));
+
+    new Setting(containerEl)
+      .setName('Supernotes Folder')
+      .setDesc('Folder used for Supernotes download/sync')
+      .addText(text => text
+        .setPlaceholder('Enter your API Key')
+        .setValue(this.plugin.settings.folder)
+        .onChange(async (value) => {
+          this.plugin.settings.folder = value;
+          await this.plugin.saveSettings();
+        }));
+  }
+}
